8ac8fac372eea9c698d0ebdf8d843cfd2a0d8902
romafrontend <rom25dan@gmail.com>
2021-08-25 18:36:09 +0300

    init app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, ChangeDetectionStrategy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styles: [``],
+  template: `
+  
+  `
 })
 export class AppComponent {
-  title = 'solidus-app';
+
 }
